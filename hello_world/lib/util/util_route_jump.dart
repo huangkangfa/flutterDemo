d644ab4e83370a418797a3649c99cd8e910bfad6
huangkf <huangkf@hiwitech.com>
2020-03-02 15:53:19 +0800

    no message

--- hello_world/lib/util/util_route_jump.dart
+++ hello_world/lib/util/util_route_jump.dart
@@ -2,18 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 push(BuildContext context, route) {
-  Navigator.push(context,
+  return Navigator.push(context,
       new MaterialPageRoute(builder: (BuildContext context) {
     return route;
   }));
 }
 
 pop(BuildContext context) {
-  Navigator.pop(context);
+  return Navigator.pop(context);
 }
 
 reset(BuildContext context, route) {
-  Navigator.of(context)
+  return Navigator.of(context)
       .pushReplacement(new MaterialPageRoute(builder: (BuildContext context) {
     return route;
   }));
